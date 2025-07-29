bed8fbae43328ae09bbce8e7368c4ffc30e32a68
hiclassic <hares.ahmed.319@gmail.com>
2025-07-30 03:22:13 +0600

    index file added

---
+++ exam_aug/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>DB Driven Web Application</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body class="p-5">
+<div class="container">
+    <h2>Welcome to Database Driven Web App</h2>
+    <div class="list-group mt-4">
+        <a href="manufacturer_crud.php" class="list-group-item list-group-item-action">
+            Manage Manufacturers
+        </a>
+        <a href="product_crud.php" class="list-group-item list-group-item-action">
+            Manage Products
+        </a>
+        <a href="view_expensive_products.php" class="list-group-item list-group-item-action">
+            View Expensive Products
+        </a>
+    </div>
+</div>
+</body>
+</html>
